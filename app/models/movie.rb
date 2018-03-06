b384276c6a1187fa59d011d5c2daf40bea4649d1
vsparrow <khan@vsparrow.com>
2018-03-06 16:25:13 -0500

    Add class methods to model Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,16 @@
 class Movie < ActiveRecord::Base
   belongs_to :director
   belongs_to :genre
+
+  def self.have_won_oscars
+    self.all.where("oscars > 0")
+  end
+
+  def self.most_oscars_won
+    self.all.maximum("oscars")
+  end
+
+  def self.title_with_most_oscars_won
+    self.all.where("oscars == ?", self.most_oscars_won)
+  end
 end
