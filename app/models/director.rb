f8210aa1321e9268765314140eb337ecb5b67a57
vsparrow <khan@vsparrow.com>
2018-03-06 17:16:31 -0500

    Add function director_with_most_movies to Director model

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,18 @@
 class Director < ActiveRecord::Base
   has_many :movies
   has_many :genres, through: :movies
+
+  def self.director_with_most_movies
+    dir_with_most_movies = nil
+    self.all.each do |d|
+      # dir_with_most_movies = d if d.movies.count > dir_with_most_movies.movies.count unless dir_with_most_movies.nil?
+      if dir_with_most_movies.nil?
+        dir_with_most_movies = d
+      end
+      if d.movies.count > dir_with_most_movies.movies.count
+        dir_with_most_movies = d
+      end
+    end
+    puts dir_with_most_movies.name
+  end
 end
