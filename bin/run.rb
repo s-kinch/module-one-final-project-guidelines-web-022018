a0b0cdb491031d5d27dd32d4f22acc8b312277bd
vsparrow <khan@vsparrow.com>
2018-03-06 18:02:52 -0500

    bin/run minimal functionality

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,35 @@
-require_relative 'config/environment'
+# require_relative 'config/environment'
+require './config/environment'
 
+
+
+
+def list_commands
+
+end
+
+def do_command(entry=nil)
+
+end
+
+
+
+#possibly clear screen first
+puts "Welcome to the Greatest Films of All Time Database"
+
+stay_in_app = true
+
+while(stay_in_app) do
+  # puts "What would you like to do? Type 'help' for a list of commands"
+  puts "Here are a list of commands. What would you like to do"
+  list_commands #function
+  entry=gets.chomp
+  # do_command(entry) if entry != "quit"#function
+  # stay_in_app = false if entry == "quit"
+  if entry == "quit"
+    stay_in_app = false
+  else
+    do_command(entry)
+  end #if
+  stay_in_app = false
+end# while
