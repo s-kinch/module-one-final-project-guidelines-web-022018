8a82b217f081c61f3854acfa1c618156c56020d7
vsparrow <khan@vsparrow.com>
2018-03-06 17:37:42 -0500

    Add function genre_with_the_most_movies to Genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,14 @@
 class Genre < ActiveRecord::Base
   has_many :movies
   has_many :directors, through: :movies
+
+  def self.genre_with_the_most_movies
+    current_top_genre= nil
+    self.all.each do |g|
+      current_top_genre = g if current_top_genre.nil?
+      current_top_genre = g if g.movies.count > current_top_genre.movies.count
+    end
+    # puts "**************TOP GENRE IS!:::#{current_top_genre.name}"
+    puts "#{current_top_genre.name}"
+  end
 end
