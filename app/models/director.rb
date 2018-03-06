caa89f5ef550771d9c598218e8c7e1889ac618f9
sinclairstudy <sinclairstudy@gmail.com>
2018-03-06 12:19:00 -0500

    added require for models in environment file

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,4 @@
 class Director < ActiveRecord::Base
-  has_many :movies
-  has_many :genres, :through :movies
+  # has_many :movies
+  # has_many :genres, :through :movies
 end
--- db/migrate/01_create_directors.rb
+++ db/migrate/01_create_directors.rb
@@ -1,4 +1,4 @@
-class CreateDirectors < ActiveRecord::Migration
+class CreateDirectors < ActiveRecord::Migration[4.2]
   def change
     create_table :directors do |t|
       t.string :name
