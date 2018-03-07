d83c00b036d45ff3cd5eb0b7d9f346eb06dc36ff
sinclairstudy <sinclairstudy@gmail.com>
2018-03-07 15:34:34 -0500

    reorganize menu

--- bin/run.rb
+++ bin/run.rb
@@ -10,24 +10,24 @@ def list_commands
     Type 'quit' to leave program
     --------------------------
     1. List All Directors
-    7. List All Movies from a Director
-    8. List All Movie Genres from a Director
-    11. List Director with the Most Movies
-    15. List Movies by Director and Genre
+    2. List All Movies from a Director
+    3. List All Movie Genres from a Director
+    4. List Director with the Most Movies
+    5. List Movies by Director and Genre
     --------------------------
-    9. List All Countries that have Movies on the List
-    10. List All Movies from a Country
+    6. List All Countries that have Movies on the List
+    7. List All Movies from a Country
     --------------------------
-    12. List All Movies that Won Oscars
-    13. List Movie with Most Oscars
-    14. List Oscars by Country
+    8. List All Movies that Won Oscars
+    9. List Movie with Most Oscars
+    10. List Oscars by Country
     --------------------------
-    3. List All Movies
-    4. List Movies from a Year
-    5. List Movies from a Range of Years
+    11. List All Movies
+    12. List Movies from a Year
+    13. List Movies from a Range of Years
     --------------------------
-    2. List All Genres
-    6. List All Movies from a Genre
+    14. List All Genres
+    15. List All Movies from a Genre
     16. List Genre with the Most Movies
     --------------------------
     17. List All Actors
@@ -42,47 +42,16 @@ def do_command(entry=nil)
   when "1"
     puts Director.director_names
   when "2"
-    puts Genre.genre_names
-  when "3"
-    Movie.movie_names
-  when "4"
-    print "Please enter year: "
-    year = gets.chomp
-    Movie.movies_within_single_year(year)
-  when "5"
-    puts "Please enter a range of years."
-    print "Enter the first year: "
-    year1 = gets.chomp
-    print "Enter the second year: "
-    year2 = gets.chomp
-    Movie.movies_within_year_range(year1, year2)
-  when "6"
-    print "Please enter a genre: "
-    genre_name = gets.chomp
-    Genre.print_list_movies_from_genre(genre_name)
-  when "7"
     print "Please enter a director: "
     director_name = gets.chomp
     Director.print_list_of_movies_by_director(director_name)
-  when "8"
+  when "3"
     print "Please enter director name:"
     name = gets.chomp
     Director.print_genres_of_movies_by_director(name)
-  when "9"
-    Movie.print_list_of_countries
-  when "10"
-    print "Please enter country name: "
-    name = gets.chomp
-    Movie.list_movies_from_a_country(name)
-  when "11"
+  when "4"
     Director.director_with_most_movies
-  when "12"
-    Movie.print_titles(Movie.have_won_oscars)
-  when "13"
-    puts Movie.movie_with_most_oscars_won[0].name + ", " + Movie.most_oscars_won.to_s
-  when "14"
-    Movie.oscars_by_country
-  when "15"
+  when "5"
     print "Please enter director name: "
     dir_name = gets.chomp
     if Director.find_by(name: dir_name)
@@ -96,6 +65,37 @@ def do_command(entry=nil)
     else
       puts "Invalid input"
     end
+  when "6"
+    Movie.print_list_of_countries
+  when "7"
+    print "Please enter country name: "
+    name = gets.chomp
+    Movie.list_movies_from_a_country(name)
+  when "8"
+    Movie.print_titles(Movie.have_won_oscars)
+  when "9"
+    puts Movie.movie_with_most_oscars_won[0].name + ", " + Movie.most_oscars_won.to_s
+  when "10"
+    Movie.oscars_by_country
+  when "11"
+    Movie.movie_names
+  when "12"
+    print "Please enter year: "
+    year = gets.chomp
+    Movie.movies_within_single_year(year)
+  when "13"
+    puts "Please enter a range of years."
+    print "Enter the first year: "
+    year1 = gets.chomp
+    print "Enter the second year: "
+    year2 = gets.chomp
+    Movie.movies_within_year_range(year1, year2)
+  when "14"
+    puts Genre.genre_names
+  when "15"
+    print "Please enter a genre: "
+    genre_name = gets.chomp
+    Genre.print_list_movies_from_genre(genre_name)
   when "16"
     Genre.genre_with_the_most_movies
   when "17"
@@ -124,8 +124,9 @@ while(stay_in_app) do
   # puts "What would you like to do? Type 'help' for a list of commands"
   puts "***********************************************************************"
   puts "***********************************************************************"
-  puts "Here are a list of commands. What would you like to do"
+  puts "Here are a list of commands. What would you like to do?"
   list_commands #function
+  print "Please enter a number or 'quit': "
   entry=gets.chomp
   # do_command(entry) if entry != "quit"#function
   # stay_in_app = false if entry == "quit"
